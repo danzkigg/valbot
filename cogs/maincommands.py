29a4f51202e55debafb114b5d113d169e4d4dad3
Daniel Silva <62973580+danzkigg@users.noreply.github.com>
2021-01-17 22:20:30 +0100

    Update maincommands.py

--- cogs/maincommands.py
+++ cogs/maincommands.py
@@ -26,7 +26,7 @@ class MainCommands(commands.Cog):
         pjdata = json.loads(pjson)
         embed = discord.Embed()
         embed.colour = 16729684
-        embed.set_thumbnail (url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/1200px-Valorant_logo_-_pink_color_version.svg.png')
+        embed.set_thumbnail (url='https://valbot.io/img/disc/games/val-logo.png')
         embed.title = (pjdata['result']['pageContext']['data']['articles'][0]['title'])
         embed.url = 'https://playvalorant.com/en-us' + (pjdata['result']['pageContext']['data']['articles'][0]['url']['url'])
         embed.description = (pjdata['result']['pageContext']['data']['articles'][0]['description'])
@@ -43,7 +43,7 @@ class MainCommands(commands.Cog):
             if data['maintenances'] == []:
                 embed = discord.Embed()
                 embed.colour = 3591856
-                embed.set_thumbnail (url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/1200px-Valorant_logo_-_pink_color_version.svg.png')
+                embed.set_thumbnail (url='https://valbot.io/img/disc/games/val-logo.png')
                 embed.title = ':green_circle: **Servers are Online**'
                 embed.description = "Valorant's European servers are currently **Online**!\n No maintenance is in progress."
                 await ctx.send(embed=embed)
@@ -52,15 +52,61 @@ class MainCommands(commands.Cog):
                 print(f'API returned a {response.status} status')
                 embed = discord.Embed()
                 embed.colour = 16746010
-                embed.set_thumbnail (url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/1200px-Valorant_logo_-_pink_color_version.svg.png')
+                embed.set_thumbnail (url='https://valbot.io/img/disc/games/val-logo.png')
                 embed.title = ':orange_circle: **Maintenance**'
                 embed.description = "Valorant's European server are currently under **Maintenance**!"
                 await ctx.send(embed=embed)
 
-
+    #Leaderboard in Valorant Europe
     @commands.command()
-    async def test(self, ctx):
-        await ctx.send ('test')
+    async def leaderboard(self, ctx):
+        API = os.getenv('RIOT_API')
+        url = 'https://eu.api.riotgames.com/val/ranked/v1/leaderboards/by-act/97b6e739-44cc-ffa7-49ad-398ba502ceb0?startIndex=0&api_key=' + API
+
+        async with request('GET', url) as response:
+            if response.status == 200:
+                data = await response.json(content_type='text/plain;charset=utf-8')
+                embed = discord.Embed()
+                embed.colour = 3591856
+                embed.set_thumbnail (url='https://valbot.io/img/disc/games/val-logo.png')
+                embed.title = 'Valorant Leaderboard'
+                embed.description = "This is currently the **TOP3** ranked players in Valorant's European server."
+                embed.add_field(name = 'Place', value= ':one:', inline = True)
+                embed.add_field(name = 'Nickname', value= data['players'][0]['gameName'], inline = True)
+                embed.add_field(name = 'Tagline', value= data['players'][0]['tagLine'], inline = True)
+                embed.add_field(name = 'Number of Wins', value= data['players'][0]['numberOfWins'], inline = True)
+                embed.add_field(name = 'Ranked Rating', value= data['players'][0]['rankedRating'], inline = True)
+                embed.add_field(name = '\u200b', value= '\u200b', inline = True)
+
+                embed.add_field(name = '\u200b', value= '\u200b', inline = False)
+
+                embed.add_field(name = 'Place', value= ':two:', inline = True)
+                embed.add_field(name = 'Nickname', value= data['players'][1]['gameName'], inline = True)
+                embed.add_field(name = 'Tagline', value= data['players'][0]['tagLine'], inline = True)
+                embed.add_field(name = 'Number of Wins', value= data['players'][1]['numberOfWins'], inline = True)
+                embed.add_field(name = 'Ranked Rating', value= data['players'][1]['rankedRating'], inline = True)
+                embed.add_field(name = '\u200b', value= '\u200b', inline = True)
+
+                embed.add_field(name = '\u200b', value= '\u200b', inline = False)
+
+                embed.add_field(name = 'Place', value= ':three:', inline = True)
+                embed.add_field(name = 'Nickname', value= data['players'][2]['gameName'], inline = True)
+                embed.add_field(name = 'Tagline', value= data['players'][0]['tagLine'], inline = True)
+                embed.add_field(name = 'Number of Wins', value= data['players'][2]['numberOfWins'], inline = True)
+                embed.add_field(name = 'Ranked Rating', value= data['players'][2]['rankedRating'], inline = True)
+                embed.add_field(name = '\u200b', value= '\u200b', inline = True)
+                await ctx.send(embed=embed)
+
+            else:
+                print(f'API returned a {response.status} status')
+                embed = discord.Embed()
+                embed.colour = 16746010
+                embed.set_thumbnail (url='https://valbot.io/img/disc/games/val-logo.png')
+                embed.title = "Opps... Can't connect"
+                embed.description = "Sorry! We've encountered an error, please try again later!"
+                await ctx.send(embed=embed)
+
+            
 
 def setup(client):
     client.add_cog(MainCommands(client))
